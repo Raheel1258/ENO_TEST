318d5ccf1d28a65128309891038efe95e5fe6894
Raheel1258 <rahilahmad1258@gmail.com>
2022-07-15 17:49:13 +0500

    finalizes code

---
+++ ios/ToastExample.h
@@ -0,0 +1,16 @@
+//
+//  ToastExample.h
+//  ENO_TEST
+//
+//  Created by Sparrow on 15/07/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ToastExample : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ios/ToastExample.m
@@ -0,0 +1,12 @@
+//
+//  ToastExample.m
+//  ENO_TEST
+//
+//  Created by Sparrow on 15/07/2022.
+//
+
+#import "ToastExample.h"
+
+@implementation ToastExample
+
+@end
